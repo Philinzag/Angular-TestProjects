b983f259fdebd77346af8d9cd0a0624961140f9f
Phil Inzag <phil.inzag@gmail.com>
2024-04-17 08:21:08 +0000

    Add test Spec for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'noAPIkey' title`, () => {
+  it(`should have the 'APIwithkeys' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('noAPIkey');
+    expect(app.title).toEqual('APIwithkeys');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, noAPIkey');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, APIwithkeys');
   });
 });
